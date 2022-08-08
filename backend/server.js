50e92be62b0e014a4244bdebb064a903113d685e
Zane Campbell <zanegitinit@gmail.com>
2022-08-08 10:09:14 -0700

    setup file structure and errorhandler

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+// Allows express to parse json data
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to hiking timeline' })
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
