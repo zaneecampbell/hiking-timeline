415e5f3efeb10d156ee8f50f38a2f2a0d4d90683
Zane Campbell <zanegitinit@gmail.com>
2022-09-15 18:19:28 -0700

    setup image upload to firebase storage and return image url

--- frontend/src/pages/TimelineEvent.jsx
+++ frontend/src/pages/TimelineEvent.jsx
@@ -26,8 +26,10 @@ import { Typography } from '@mui/material'
 
 function TimelineEvent() {
   const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false)
   const { timeline, isLoading, isError, isSuccess, message, isGot } =
     useSelector(state => state.timeline)
+  const { user } = useSelector(state => state.auth)
 
   const [formData, setFormData] = useState({ images: {} })
 
@@ -63,13 +65,65 @@ function TimelineEvent() {
 
   const onImageUpload = async e => {
     e.preventDefault()
-    console.log(images)
+
+    // Store image in firebase
+    const storeImage = async image => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage()
+        const fileName = `${user._id}-${uuidv4()}`
+        console.log(fileName)
+
+        const storageRef = ref(storage, 'images/' + fileName)
+
+        const uploadTask = uploadBytesResumable(storageRef, image)
+
+        uploadTask.on(
+          'state_changed',
+          snapshot => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            console.log('Upload is ' + progress + '% done')
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused')
+                break
+              case 'running':
+                console.log('Upload is running')
+                break
+              default:
+                break
+            }
+          },
+          error => {
+            reject(error)
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+              resolve(downloadURL)
+            })
+          }
+        )
+      })
+    }
+
+    const imgUrls = await Promise.all(
+      [...images].map(image => storeImage(image))
+    ).catch(() => {
+      setLoading(false)
+      toast.error('Images not uploaded')
+      return
+    })
+
+    // got Imgurls time to put them in the timeline event mongoDB part
+    console.log(imgUrls)
   }
 
   return (
     <div>
       {when} - {where}
-      <form>
+      <form onSubmit={onImageUpload}>
         <Input
           style={{ display: 'none' }}
           id='images'
@@ -94,7 +148,7 @@ function TimelineEvent() {
             <div></div>
           )}
         </label>
-        <Button onClick={onImageUpload}>Upload</Button>
+        <Button type='submit'>Upload</Button>
       </form>
     </div>
   )
