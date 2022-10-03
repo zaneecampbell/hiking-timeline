b45e2e00cb83cf0de7e6b3137c4d74e15b4239ed
Zane Campbell <zanegitinit@gmail.com>
2022-10-03 11:51:56 -0700

    deployment attempt 1

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,14 @@ app.use('/api/timeline', require('./routes/timelineRoutes'))
 
 app.use(errorHandler)
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
