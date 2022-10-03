3396eb8dbf3fd78d239412f0bb34bcb142191002
Zane Campbell <zanegitinit@gmail.com>
2022-10-03 12:26:27 -0700

    7

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   )
 } else {
   app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to hiking timeline' })
+    res.json({ message: 'Welcome to hiking timeline by Zane' })
   })
 }
 
